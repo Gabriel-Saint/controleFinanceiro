f6ef323571463412f79c58f02bcc36b7094bdcba
Gabriel - Saint <gabrielsantosayres@gmail.com>
2023-10-20 10:35:22 -0300

    Inicio da paginação

--- config/routes.js
+++ config/routes.js
@@ -6,14 +6,16 @@ const homeController = require('../src/controllers/homeController');
 
 //models
 const cadastrarModel = require('../src/models/cadastrarModel');
-const buscarDadosSQL = require('../src/models/buscarDadosSQL');
+/*const buscarDadosSQL = require('../src/models/buscarDadosSQL');*/
+const buscarDadosSQLteste = require('../src/models/buscarDadosSQLteste');
 
 const apagarDados = require('../src/models/apagarDados')
 
 router.get('/', homeController.exibirHome);
-router.get('/buscar-dados', buscarDadosSQL.buscarDadosSQL)
+//router.get('/buscar-dados', buscarDadosSQL.buscarDadosSQL)
+router.get('/buscar-dados/:limit/:offset', buscarDadosSQLteste.buscarDadosSQL)
 
-router.get('/buscar-valores', buscarDadosSQL.buscarValores)
+//router.get('/buscar-valores', buscarDadosSQL.buscarValores)
 
 router.post('/registrar', cadastrarModel.processarRegistro);
 
---
+++ src/models/buscarDadosSQLteste.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const db = require('../../config/database');
+
+function buscarDadosSQL(req, res){
+    
+  let {limit, offset} = req.params;
+
+  limit = Number(limit);
+  offset = Number(offset);
+
+  if(!limit){
+    limit = 20;
+  }
+  if(!offset){
+    offset = 1;
+  }
+
+    const sql = `CALL GetRegistros(${offset},${limit})`; 
+
+
+    db.query(sql, (err, results) => {
+        if (err) {
+          console.error('Erro ao consultar registros:', err);
+          res.status(500).send('Erro ao consultar registros.');
+        }
+        console.log('consulta realizada com ssucesso no banco de dados.');
+        res.json(results);
+      });
+  
+}
+
+function buscarValores(req, res){
+    
+  // sql
+  const sql = 'SELECT SUM(CASE WHEN tipo = 1 THEN valor ELSE 0 END) AS total_entradas, SUM(CASE WHEN tipo = 0 THEN valor ELSE 0 END) AS total_saidas, SUM(CASE WHEN tipo = 1 THEN valor ELSE -valor END) AS saldo FROM registros';
+
+  db.query(sql, (err, results) => {
+      if (err) {
+        console.error('Erro ao consultar registros:', err);
+        res.status(500).send('Erro ao consultar registros.');
+        return;
+      }
+  
+      console.log('consulta realizada com sucesso no banco de dados.');
+      //res.status(200).render(caminhoAbsoluto);
+      res.json(results);
+    });
+  
+
+
+}
+
+module.exports = {
+    buscarDadosSQL: buscarDadosSQL,
+    buscarValores: buscarValores
+};
